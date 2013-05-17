cbb768f61f90aa602c3d2a5887b858d6b919386d
nacho <nacho@linuxmint.linuxmint-domain>
2013-05-17 01:44:58 -0300

    un par de cosas

---
+++ src/main/sucursal/Caja.java
@@ -0,0 +1,12 @@
+package sucursal;
+
+public class Caja {
+	public void abrirCaja(){}
+	public void iniciarCompra(){}
+	public void agregarProductos(){}
+	public void visualizarTotal(){}
+	public void visualizarDescuentosAplicados(){}
+	public void indicarMedioDePago(){}
+	public void confirmarCompra(){}
+	public void cerrarCaja() throws CajaNoInicializadaException{}
+}
---
+++ src/main/sucursal/CajaNoInicializadaException.java
@@ -0,0 +1,30 @@
+package sucursal;
+
+public class CajaNoInicializadaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CajaNoInicializadaException() {
+}
+
+	public CajaNoInicializadaException(String message) {
+		super(message);
+	}
+
+	public CajaNoInicializadaException(Throwable cause) {
+		super(cause);
+	}
+
+	public CajaNoInicializadaException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CajaNoInicializadaException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
