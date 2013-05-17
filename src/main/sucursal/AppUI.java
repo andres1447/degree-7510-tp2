b2c700a49364e4072e9b3b04050b1be9b65a358c
Pablo.Ang <pablo.angelani@gmail.com>
2013-05-17 16:20:25 -0300

    Primera parte de la UI simple. Falta completar la parte de la compra

---
+++ src/main/sucursal/AppUI.java
@@ -0,0 +1,244 @@
+package sucursal;
+
+import java.awt.CardLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import sucursal.exceptions.CajaNoInicializadaException;
+import sucursal.exceptions.CajaYaAbiertaException;
+import sucursal.exceptions.CompraEnProcesoException;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class AppUI {
+	private final static String CAJA_CERRADA_PANEL = "CAJA_CERRADA_PANEL";
+	private final static String CAJA_ABIERTA_PANEL = "CAJA_ABIERTA_PANEL";
+	private final static String COMPRA_PANEL = "COMPRA_PANEL";
+
+	private JFrame frmCaja;
+
+	private JPanel pnlCajaCerrada;
+	private JButton btnAbrirCaja;
+
+	private JPanel pnlCajaAbierta;
+	private JButton btnIniciarCompra;
+	private JButton btnCerrarCaja;
+
+	private JPanel pnlCompra;
+	private JButton btnCancelarCompra;
+	private JButton btnConfirmarCompra;
+	private Caja caja;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Caja c = new Caja();
+					AppUI window = new AppUI(c);
+					window.frmCaja.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppUI(Caja caja) {
+		this.caja = caja;
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmCaja = new JFrame();
+		frmCaja.setTitle("Caja");
+		frmCaja.setResizable(false);
+		frmCaja.setBounds(100, 100, 700, 400);
+		frmCaja.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCaja.getContentPane().setLayout(new CardLayout(0, 0));
+		frmCaja.getContentPane().add(getPnlCajaCerrada(), CAJA_CERRADA_PANEL);
+		frmCaja.getContentPane().add(getPnlCajaAbierta(), CAJA_ABIERTA_PANEL);
+		frmCaja.getContentPane().add(getPnlCompra(), COMPRA_PANEL);
+	}
+
+	private void showCajaCerrada() {
+		((CardLayout) frmCaja.getContentPane().getLayout()).show(
+				this.frmCaja.getContentPane(), CAJA_CERRADA_PANEL);
+
+	}
+
+	private void showCajaAbierta() {
+		((CardLayout) frmCaja.getContentPane().getLayout()).show(
+				this.frmCaja.getContentPane(), CAJA_ABIERTA_PANEL);
+
+	}
+
+	private void showCompra() {
+		((CardLayout) frmCaja.getContentPane().getLayout()).show(
+				this.frmCaja.getContentPane(), COMPRA_PANEL);
+
+	}
+
+	private Container getPnlCajaCerrada() {
+		if (pnlCajaCerrada == null) {
+			pnlCajaCerrada = new JPanel();
+			pnlCajaCerrada.setLayout(new FormLayout(new ColumnSpec[] {
+					ColumnSpec.decode("default:grow"),},
+				new RowSpec[] {
+					FormFactory.LINE_GAP_ROWSPEC,
+					RowSpec.decode("default:grow"),}));
+			pnlCajaCerrada.add(getBtnAbrirCaja(), "1, 2, center, center");
+		}
+		return pnlCajaCerrada;
+	}
+
+	private JButton getBtnAbrirCaja() {
+		if (btnAbrirCaja == null) {
+			btnAbrirCaja = new JButton("Abrir Caja");
+			btnAbrirCaja.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					try {
+						caja.abrirCaja();
+						showCajaAbierta();
+					} catch (CajaYaAbiertaException e) {
+						JOptionPane.showMessageDialog(null,
+								"La caja ya se encuentra abierta.", "Error",
+								JOptionPane.WARNING_MESSAGE);
+					}
+				}
+			});
+		}
+		return btnAbrirCaja;
+	}
+
+	private Container getPnlCajaAbierta() {
+		if (pnlCajaAbierta == null) {
+			pnlCajaAbierta = new JPanel();
+			pnlCajaAbierta.setLayout(new FormLayout(new ColumnSpec[] {
+					ColumnSpec.decode("default:grow"),
+					FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+					ColumnSpec.decode("default:grow"),},
+				new RowSpec[] {
+					FormFactory.LINE_GAP_ROWSPEC,
+					RowSpec.decode("default:grow"),}));
+			pnlCajaAbierta.add(getBtnCerrarCaja(), "1, 2, center, center");
+			pnlCajaAbierta.add(getBtnIniciarCompra(), "3, 2, center, center");
+
+		}
+		return pnlCajaAbierta;
+	}
+
+	private Component getBtnIniciarCompra() {
+		if (btnIniciarCompra == null) {
+			btnIniciarCompra = new JButton("Iniciar Compra");
+			btnIniciarCompra.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					try {
+						caja.iniciarCompra();
+						showCompra();
+					} catch (CompraEnProcesoException e) {
+						JOptionPane.showMessageDialog(null,
+								"Compra en proceso.", "Error",
+								JOptionPane.WARNING_MESSAGE);
+					}
+				}
+			});
+		}
+		return btnIniciarCompra;
+	}
+
+	private JButton getBtnCerrarCaja() {
+		if (btnCerrarCaja == null) {
+			btnCerrarCaja = new JButton("Cerrar Caja");
+			btnCerrarCaja.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					try {
+						caja.cerrarCaja();
+						showCajaCerrada();
+					} catch (CajaNoInicializadaException e) {
+						JOptionPane.showMessageDialog(null,
+								"La caja ya se encuentra cerrada.", "Error",
+								JOptionPane.WARNING_MESSAGE);
+					}
+				}
+			});
+		}
+		return btnCerrarCaja;
+	}
+
+	private JPanel getPnlCompra() {
+		if (pnlCompra == null) {
+			pnlCompra = new JPanel();
+			pnlCompra.setLayout(new FormLayout(new ColumnSpec[] {
+					FormFactory.UNRELATED_GAP_COLSPEC,
+					ColumnSpec.decode("max(200dlu;min):grow"),
+					FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+					FormFactory.GROWING_BUTTON_COLSPEC,
+					FormFactory.UNRELATED_GAP_COLSPEC, }, new RowSpec[] {
+					FormFactory.UNRELATED_GAP_ROWSPEC,
+					FormFactory.DEFAULT_ROWSPEC,
+					FormFactory.RELATED_GAP_ROWSPEC,
+					RowSpec.decode("default:grow"),
+					FormFactory.RELATED_GAP_ROWSPEC,
+					FormFactory.DEFAULT_ROWSPEC,
+					FormFactory.UNRELATED_GAP_ROWSPEC, }));
+			pnlCompra.add(getBtnCancelarCompra(), "4, 2, right, default");
+			pnlCompra.add(new CompraUI(caja), "2, 4, 3, 1, fill, fill");
+			pnlCompra.add(getBtnConfirmarCompra(), "4, 6, right, default");
+
+		}
+		return pnlCompra;
+	}
+
+	private JButton getBtnConfirmarCompra() {
+		if (btnConfirmarCompra == null) {
+			btnConfirmarCompra = new JButton("Confirmar Compra");
+			btnConfirmarCompra.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					caja.confirmarCompra();
+					showCajaAbierta();
+				}
+			});
+		}
+		return btnConfirmarCompra;
+	}
+
+	private JButton getBtnCancelarCompra() {
+		if (btnCancelarCompra == null) {
+			btnCancelarCompra = new JButton("Cancelar Compra");
+			btnCancelarCompra.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					showCajaAbierta();
+					/*
+					 * try { caja.cancelarCompra(); } catch
+					 * (CajaNoInicializadaException e) {
+					 * JOptionPane.showMessageDialog(null,
+					 * "La caja ya se encuentra cerrada.", "Error",
+					 * JOptionPane.WARNING_MESSAGE); }
+					 */
+				}
+			});
+		}
+		return btnCancelarCompra;
+	}
+
+}
---
+++ src/main/sucursal/CompraUI.java
@@ -0,0 +1,210 @@
+package sucursal;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class CompraUI extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	private JPanel pnlDescProducto;
+	private JScrollPane scrollPane;
+	private JTable table;
+
+	private JButton btnAgregarProducto;
+	private JButton btnBorrarProducto;
+
+	private JLabel lblNombreProducto;
+	private JLabel lblMarcaProducto;
+	private JLabel lblCodigoProducto;
+	private JTextPane tpnDescripcionProducto;
+	private JLabel lblRubroProducto;
+
+	private Caja caja;
+
+	/**
+	 * Create the frame.
+	 */
+	public CompraUI(Caja caja) {
+		this.caja = caja;
+		initComponents();
+	}
+
+	private void initComponents() {
+		setBounds(0, 0, 700, 400);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("max(200dlu;min):grow"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				FormFactory.GROWING_BUTTON_COLSPEC,},
+			new RowSpec[] {
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		add(getScrollPane(), "1, 1, 1, 5, fill, fill");
+		add(getPnlDescProducto(), "3, 1, fill, fill");
+		add(getBtnBorrarProducto(), "3, 3");
+		add(getBtnAgregarProducto(), "1, 7");
+	}
+
+	private JButton getBtnAgregarProducto() {
+		if (btnAgregarProducto == null) {
+			btnAgregarProducto = new JButton("Agregar Producto");
+		}
+		return btnAgregarProducto;
+	}
+
+	private JButton getBtnBorrarProducto() {
+		if (btnBorrarProducto == null) {
+			btnBorrarProducto = new JButton("Borrar Producto");
+		}
+		return btnBorrarProducto;
+	}
+
+	private JPanel getPnlDescProducto() {
+		if (pnlDescProducto == null) {
+			pnlDescProducto = new JPanel();
+			GridBagLayout gbl_pnlDescProducto = new GridBagLayout();
+			gbl_pnlDescProducto.columnWidths = new int[] { 53, 73, 0 };
+			gbl_pnlDescProducto.rowHeights = new int[] { 15, 0, 0, 0, 0, 0, 0 };
+			gbl_pnlDescProducto.columnWeights = new double[] { 1.0, 0.0,
+					Double.MIN_VALUE };
+			gbl_pnlDescProducto.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0,
+					0.0, 1.0, Double.MIN_VALUE };
+			pnlDescProducto.setLayout(gbl_pnlDescProducto);
+			GridBagConstraints gbc_lblProducto = new GridBagConstraints();
+			gbc_lblProducto.insets = new Insets(0, 0, 5, 5);
+			gbc_lblProducto.anchor = GridBagConstraints.NORTHWEST;
+			gbc_lblProducto.gridx = 0;
+			gbc_lblProducto.gridy = 0;
+			pnlDescProducto.add(new JLabel("Producto:"), gbc_lblProducto);
+			GridBagConstraints gbc_lblNombreProducto = new GridBagConstraints();
+			gbc_lblNombreProducto.insets = new Insets(0, 0, 5, 0);
+			gbc_lblNombreProducto.gridx = 1;
+			gbc_lblNombreProducto.gridy = 0;
+			pnlDescProducto.add(getLblNombreProducto(), gbc_lblNombreProducto);
+			GridBagConstraints gbc_lblMarca = new GridBagConstraints();
+			gbc_lblMarca.anchor = GridBagConstraints.WEST;
+			gbc_lblMarca.insets = new Insets(0, 0, 5, 5);
+			gbc_lblMarca.gridx = 0;
+			gbc_lblMarca.gridy = 1;
+			pnlDescProducto.add(new JLabel("Marca:"), gbc_lblMarca);
+			GridBagConstraints gbc_lblMarcaProducto = new GridBagConstraints();
+			gbc_lblMarcaProducto.insets = new Insets(0, 0, 5, 0);
+			gbc_lblMarcaProducto.gridx = 1;
+			gbc_lblMarcaProducto.gridy = 1;
+			pnlDescProducto.add(getLblMarcaProducto(), gbc_lblMarcaProducto);
+			GridBagConstraints gbc_lblCodigo = new GridBagConstraints();
+			gbc_lblCodigo.anchor = GridBagConstraints.WEST;
+			gbc_lblCodigo.insets = new Insets(0, 0, 5, 5);
+			gbc_lblCodigo.gridx = 0;
+			gbc_lblCodigo.gridy = 2;
+			pnlDescProducto.add(new JLabel("Codigo:"), gbc_lblCodigo);
+			GridBagConstraints gbc_lblCodigoProducto = new GridBagConstraints();
+			gbc_lblCodigoProducto.insets = new Insets(0, 0, 5, 0);
+			gbc_lblCodigoProducto.gridx = 1;
+			gbc_lblCodigoProducto.gridy = 2;
+			pnlDescProducto.add(getLblCodigoProducto(), gbc_lblCodigoProducto);
+			GridBagConstraints gbc_lblRubro = new GridBagConstraints();
+			gbc_lblRubro.anchor = GridBagConstraints.WEST;
+			gbc_lblRubro.insets = new Insets(0, 0, 5, 5);
+			gbc_lblRubro.gridx = 0;
+			gbc_lblRubro.gridy = 3;
+			pnlDescProducto.add(new JLabel("Rubro:"), gbc_lblRubro);
+			GridBagConstraints gbc_lblRubroProducto = new GridBagConstraints();
+			gbc_lblRubroProducto.insets = new Insets(0, 0, 5, 0);
+			gbc_lblRubroProducto.gridx = 1;
+			gbc_lblRubroProducto.gridy = 3;
+			pnlDescProducto.add(getLblRubroProducto(), gbc_lblRubroProducto);
+			GridBagConstraints gbc_lblDescripcin = new GridBagConstraints();
+			gbc_lblDescripcin.anchor = GridBagConstraints.WEST;
+			gbc_lblDescripcin.insets = new Insets(0, 0, 5, 5);
+			gbc_lblDescripcin.gridx = 0;
+			gbc_lblDescripcin.gridy = 4;
+			pnlDescProducto.add(new JLabel("Descripcion:"), gbc_lblDescripcin);
+			GridBagConstraints gbc_tpnDescripcionProducto = new GridBagConstraints();
+			gbc_tpnDescripcionProducto.gridwidth = 2;
+			gbc_tpnDescripcionProducto.fill = GridBagConstraints.BOTH;
+			gbc_tpnDescripcionProducto.gridx = 0;
+			gbc_tpnDescripcionProducto.gridy = 5;
+			pnlDescProducto.add(getTpnDescripcionProducto(),
+					gbc_tpnDescripcionProducto);
+		}
+		return pnlDescProducto;
+	}
+
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setViewportView(getTable());
+		}
+		return scrollPane;
+	}
+
+	private JTable getTable() {
+		if (table == null) {
+			table = new JTable();
+			DefaultTableModel model = new DefaultTableModel(new Object[][] {},
+					new String[] { "Cantidad", "Producto", "Precio Unid.",
+							"Total" });
+			table.setModel(model);
+			table.setFillsViewportHeight(true);
+		}
+		return table;
+	}
+
+	private JLabel getLblNombreProducto() {
+		if (lblNombreProducto == null) {
+			lblNombreProducto = new JLabel("");
+		}
+		return lblNombreProducto;
+	}
+
+	private JLabel getLblMarcaProducto() {
+		if (lblMarcaProducto == null) {
+			lblMarcaProducto = new JLabel("");
+		}
+		return lblMarcaProducto;
+	}
+
+	private JLabel getLblCodigoProducto() {
+		if (lblCodigoProducto == null) {
+			lblCodigoProducto = new JLabel("");
+		}
+		return lblCodigoProducto;
+	}
+
+	private JLabel getLblRubroProducto() {
+		if (lblRubroProducto == null) {
+			lblRubroProducto = new JLabel("");
+		}
+		return lblRubroProducto;
+	}
+
+	private JTextPane getTpnDescripcionProducto() {
+		if (tpnDescripcionProducto == null) {
+			tpnDescripcionProducto = new JTextPane();
+		}
+		return tpnDescripcionProducto;
+	}
+
+}
