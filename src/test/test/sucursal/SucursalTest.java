19f0f32fdcf2d4d002652a956db679e29cb4f28b
nacho <nacho@linuxmint.linuxmint-domain>
2013-05-17 16:02:30 -0300

    Agregado mini esqueleto de pruebas para Sucursal y Oferta, falta completar y MUCHO

---
+++ src/test/test/sucursal/SucursalTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package test.sucursal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author nacho
+ *
+ */
+public class SucursalTest {
+
+	/**
+	 * Test contra una unica oferta asociada a una marca
+	 */
+	
+	@Test
+	public void testGetUnicaOfertaMarca() {
+		
+	}
+	/**
+	 * Test contra una unica oferta asociada a un rubro
+	 */
+	@Test
+	public void testGetUnicaOfertaRubro() {
+		
+	}
+	
+	/**
+	 * Test contra una unica oferta asociada a un producto
+	 */
+	@Test
+	public void testGetUnicaOfertaProducto() {
+		
+	}
+	
+}
